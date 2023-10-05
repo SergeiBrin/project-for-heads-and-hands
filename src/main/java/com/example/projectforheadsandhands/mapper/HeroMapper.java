d13797061b32db15c15e0799001f92e9543021b5
Sergei <starodumovtmt@yandex.ru>
2023-10-05 16:39:48 +0500

    feat: add a mapper for a Hero

--- src/main/java/com/example/projectforheadsandhands/mapper/HeroMapper.java
+++ src/main/java/com/example/projectforheadsandhands/mapper/HeroMapper.java
@@ -1,9 +1,30 @@
 package com.example.projectforheadsandhands.mapper;
 
 import com.example.projectforheadsandhands.model.Hero;
+import com.example.projectforheadsandhands.model.dto.HeroNameDto;
 import com.example.projectforheadsandhands.model.dto.ShortHeroDto;
 
 public class HeroMapper {
+
+    public static Hero buildHero(HeroNameDto heroNameDto,
+                                 int attack,
+                                 int defence,
+                                 int health,
+                                 int lowDamage,
+                                 int highDamage,
+                                 int recovery) {
+
+        return Hero.builder()
+                .name(heroNameDto.getName())
+                .attack(attack)
+                .defense(defence)
+                .health(health)
+                .originalHealth(health)
+                .lowDamage(lowDamage)
+                .highDamage(highDamage)
+                .recovery(recovery)
+                .build();
+    }
 
     public static ShortHeroDto buildShortHeroDto(Hero hero) {
         return ShortHeroDto.builder()
